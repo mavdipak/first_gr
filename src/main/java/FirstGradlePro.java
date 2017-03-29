c29b28069266bd05fce580b0fba04a30cb5d58e4
dipakmav <mavdipak@gmail.com>
2017-03-30 04:08:15 +0530

    first commit

---
+++ src/main/java/FirstGradlePro.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Vector;
+
+/**
+ * Created by dipakk on 3/29/2017.
+ */
+public class FirstGradlePro {
+    public static void main(String args[]){
+        System.out.println("hey dude wassup\n");
+        ArrayList al = new ArrayList();
+        Vector vl = new Vector();
+        System.out.println("Default capacity of vector "+vl.capacity());
+        System.out.println("Default capacity of arraylist "+al.size());
+    }
+}
